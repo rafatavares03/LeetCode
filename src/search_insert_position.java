2a8a6c0a49431f570b7b80c8526b12f7faffbf8f
Rafael Tavares <rafatavares03@hotmail.com>
2024-11-05 23:58:00 -0300

    Search Insert Position

---
+++ src/search_insert_position.java
@@ -0,0 +1,16 @@
+class Solution {
+    int binarySearch(int[] array, int searchNumber, int begin, int end) {
+        int middleNumber = array[(begin+end)/2];
+        if(middleNumber == searchNumber) return (begin+end)/2;
+        if(begin >= end) {
+            if(searchNumber > array[begin]) return begin + 1;
+            return begin;
+        }
+        if (searchNumber > middleNumber) return binarySearch(array, searchNumber, (begin+end)/2 + 1, end);
+        return binarySearch(array, searchNumber, begin, (begin + end) / 2 - 1);
+    }
+
+    public int searchInsert(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+}
